a0291b4af0985058c0b6b9736723b5b04c56117e
MartyBackFly <104285670+MartyBackFly@users.noreply.github.com>
2023-05-31 21:10:59 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+/*    
+var  index =0;
+
+showimag (1);
+function plusindex(n){
+    
+    index =index + 1 ;
+    showimag(index);
+}
+
+function showimag(n){
+var i ;
+var x = document.getElementsByClassName("slides");
+
+if(n>x.length ) {index=1 };
+if(n<1) {index=x.length };
+for(i=0;i<x.length;i++)
+{  
+  x[i].style.display = "none";
+  }
+  
+  
+    x[index-1].style.display ="block";
+    
+} 
+
+
+autoslide();
+
+function  autoslide(){
+  
+ var i ;
+var x = document.getElementsByClassName("slides");
+settimeout (autoslide,2000);
+  
+} */
